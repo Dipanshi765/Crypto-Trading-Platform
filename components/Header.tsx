3d00ac1f0606575f8af9e1c081b4da1703b24ee3
Dipanshi <dipanshi.diwakar765@gmail.com>
2026-06-08 23:38:31 +0530

    completed navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+
 'use client';
+import React from 'react'
 
 import Link from "next/link";
 import Image from "next/image";
 import {usePathname} from "next/navigation";
+import { cn } from '@/lib/utils';
 
 const Header = () => {
     const pathname = usePathname();
@@ -11,14 +13,21 @@ const Header = () => {
     <header>
         <div className="main-container inner">
             <Link href="/">
-            <Image src="logo.svg" alt="coinPulse logo" width={132} height={40} />
+            <Image src="/logo.svg" alt="coinPulse logo" width={132} height={40} />
             </Link>
 
             <nav>
-                <Link href ='/' className={}>Home</Link>
+                <Link href ='/' className={cn('nav-link',{
+                    'is-active': pathname === '/' , 
+                    'is-home' : true
+                })}>Home</Link>
 
                 <p>Search Modal</p>
-                <Link href="/coins">All Coins</Link>
+
+                <Link href="/coins" className={cn('nav-link',{
+                    'is-active': pathname === '/coins' , 
+                    
+                })}>All Coins</Link>
             </nav>
 
         </div>
